6475eb7fe690fd9e83f12b7c46970f30b1fdd2c6
Farruh Mavlonov <farruhnickspeedy@gmail.com>
2019-10-22 20:53:20 +0800

    rohl is the superior coder

--- farruh.c
+++ farruh.c
@@ -7,6 +7,7 @@
 #include "helper_functions.c"
 
 void print_matrix(int *matrix, int size);
+void print_array(int *sub_array, int size, int np);
 void floyd(int *sub_array, int size, int pid, int np);
 int owner(int k, int np, int size);
 void copy_row(int *sub_array, int size, int np, int *row_k, int k);
@@ -55,85 +56,33 @@ int main(int argc, char* argv[]) {
     //parse adjacency matrix (zeroes are converted to 'infinity')
     MatrixContainer matrix_container = get_Matrix(argv[1], size);
     matrix = matrix_container.matrix;
-    print_matrix(matrix, size);
+    //print_matrix(matrix, size);
   }
 
   double t_start = MPI_Wtime();
   //---------------PARTITION MATRIX------------------
-  sub_array = malloc(num_local_elements * sizeof(int)) ;
+  sub_array = malloc(num_local_elements * sizeof(int));
   MPI_Scatter(&matrix[lo], num_local_elements, MPI_INT, sub_array, num_local_elements, MPI_INT, 0, MPI_COMM_WORLD);
   int global_index = lo + num_local_elements*pid;
-  printf("Process %d has %d elements starting at index %d\n", pid, num_local_elements, global_index);
+  //printf("Process %d has %d elements starting at index %d\n", pid, num_local_elements, global_index);
 
-  //-------------SEND LEFT OVERS TO ROOT-------------
-  char buf[20];
-  if(pid == 0) {
-    if (lo > 0) {
-      leftovers = malloc(lo * sizeof(int));
-      for (i = 0; i < lo; i++) {
-        leftovers[i] = matrix[i];
-      }
-    }
+
+  floyd(sub_array, size, pid, np);
+
+  int *temp_matrix = malloc((size * size) * sizeof(int));
+  MPI_Gather(sub_array, num_local_elements, MPI_INT, temp_matrix, num_local_elements, MPI_INT, 0, MPI_COMM_WORLD);
+
+  if (pid == 0) {
+    print_matrix(temp_matrix, size);
   }
 
-  //--------------LOOPING OVER ITERATIONS--------------
-  for(k = 0; k<size; k++) {
-    //--------------RUN ALGORITHMN ON LEFTOVERS-----------
-    if(pid==0) {
-      if (lo>0) {
-        snprintf(buf, 20, "BEFORE_sub_array_%d", -1); // puts string into buffer
-        //print_int_array(leftovers,lo,buf);
-        leftovers = update_local_array_with_matrix(leftovers, 0, lo, k, matrix, size);
-        snprintf(buf, 20, "AFTER_sub_array_%d", -1); // puts string into buffer
-        //print_int_array(leftovers,lo,buf);
-      }
-    }
-
-    //--------------RUN ALGORITHMN ON SUBARRAYS-----------
-    snprintf(buf, 20, "BEFORE_sub_array_%d", pid); // puts string into buffer
-    //print_int_array(sub_array,num_local_elements,buf);
-    sub_array = update_local_array_with_matrix(sub_array, global_index, num_local_elements, k, matrix, size);
-    snprintf(buf, 20, "AFTER_sub_array_%d", pid); // puts string into buffer
-    //print_int_array(sub_array,num_local_elements,buf);
-
-    MPI_Barrier(MPI_COMM_WORLD);
-    int* result = malloc(sizeof(int) * np * num_local_elements);
-  }
-
-  //   //--------------MERGE ITERATION RESULT--------------
-  //   MPI_Gather(sub_array, num_local_elements, MPI_INT, result, num_local_elements, MPI_INT, 0, MPI_COMM_WORLD);
-  //   if (pid==0) {
-  //     matrix = merge_scattered_arrays(matrix, leftovers, lo, result, size);
-  //   }
-  //
-  //   //--------------SHARE RESULT TO ALL PROCESSES-----------
-  //   MPI_Bcast(matrix, size*size, MPI_INT, 0, MPI_COMM_WORLD);
-  //
-  // }
-  //
-  // //--------------PRINT RESULT--------------
-  // if (pid==0) {
-  //   //LOGGING
-  //   //Create File Name
-  //   printf("logging...");
-  //   time_t t = time(NULL);
-  //   struct tm tm = *localtime(&t);
-  //   char output[50];
-  //   sprintf(output,"outputs/%dvertices_%d%d%d_%d%d_%dprocesses.out",size,tm.tm_mday,tm.tm_mon + 1, tm.tm_year + 1900,tm.tm_hour,tm.tm_min,np);
-  //   FILE* fp = fopen(output, "w");
-  //   print_matrix(matrix, size, fp);
-  //   fclose(fp);
-  //   printf("done\n");
-  //
-  //   double runtime = MPI_Wtime() - t_start;
-  //   printf("Time taken is %f seconds\n", runtime);
-  //   //free(result);
-  //   free(matrix);
-  // }
-  // free(sub_array);
+  free(temp_matrix);
+  free(sub_array);
 
   MPI_Barrier(MPI_COMM_WORLD);
   MPI_Finalize();
+
+  return 0;
 }
 
 void print_matrix(int *matrix, int size) {
@@ -149,6 +98,17 @@ void print_matrix(int *matrix, int size) {
   }
 }
 
+void print_array(int *sub_array, int size, int np) {
+  for (i = 0; i < (size * size)/np; i++) {
+    if (sub_array[i] == INFINITY)
+      printf("i ");
+    else
+      printf("%d ", sub_array[i]);
+  }
+  printf("\n");
+}
+
+// Distributed version of Floyd Marshall algorithm
 void floyd(int *sub_array, int size, int pid, int np) {
   int global_k, local_i, global_j, temp;
   int root;
@@ -167,6 +127,7 @@ void floyd(int *sub_array, int size, int pid, int np) {
            sub_array[local_i * size + global_j] = temp;
       }
   }
+  free(row_k);
 }
 
 // Reurn the rank of the process that owns global row k
@@ -176,7 +137,7 @@ int owner(int k, int np, int size) {
 }
 
 // Copy the row with the global subscript k into row_k
-void copy_row(int *sub_array, int size, int np, int *row_k, int k) {
+void copy_row(int *sub_array, int size, int np, int *row_k, int global_k) {
   int j;
   int local_k = k % (size/np);
 
@@ -184,7 +145,3 @@ void copy_row(int *sub_array, int size, int np, int *row_k, int k) {
     row_k[j] = sub_array[local_k * size + j];
   }
 }
-
-
-//MPI_Scatter(global_matrix, num_elements_per_proc, MPI_INT, local_array, num_elements_per_proc, MPI_INT, 0, comm);
-//MPI_Gather(local_array, num_elements_per_proc, MPI_INT, global_matrix, num_elements_per_proc, MPI_INT, 0, comm);
