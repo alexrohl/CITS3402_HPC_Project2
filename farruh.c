0a28a91de0693df0679d17c5abe9a74f20c68078
Farruh Mavlonov <farruhnickspeedy@gmail.com>
2019-10-24 17:16:09 +0800

    excel data, report updated, farruh.c progress

--- farruh.c
+++ farruh.c
@@ -3,14 +3,22 @@
 #include <string.h>
 #include <mpi.h>
 #include <assert.h>
-#include "parse_input.c"
-#include "helper_functions.c"
 
-void print_matrix(int *matrix, int size);
+#define INFINITY 1000000
+#define MAX_LINE_LEN 256
+
+#define BLOCK_LOW(id,p,n) ((id)*(n)/(p))
+#define BLOCK_HIGH(id,p,n) ((id+1)*(n)/(p) - 1)
+#define BLOCK_SIZE(id,p,n) ((id+1)*(n)/(p) - (id)*(n)/(p))
+#define BLOCK_OWNER(index,p,n) (((p)*((index)+1)-1)/(n))
+
+void floyd(int **matrix, int n, int pid, int p);
+int **parse_Matrix(char *filename);
+void print_Matrix(int **matrix, int size);
 void print_array(int *sub_array, int size, int np);
-void floyd(int *sub_array, int size, int pid, int np);
-int owner(int k, int np, int size);
-void copy_row(int *sub_array, int size, int np, int *row_k, int k);
+int min(int a, int b);
+
+
 
 int main(int argc, char* argv[]) {
   if(argc == 1) {
@@ -20,64 +28,35 @@ int main(int argc, char* argv[]) {
   }
 
   int i, j, k;
-  int pid, np, size;
-  int num_elements, num_local_elements, lo;  //lo: left overs
-  int *matrix, *sub_array, *leftovers;
-  FILE *fp;
+  int pid, np, n, root = 0;
+  char *filename = argv[1];
 
   // ---INITIALIZE MPI---
+  MPI_Status status;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &pid);
   MPI_Comm_size(MPI_COMM_WORLD, &np);
 
-  // ----get relevant sizes----
-  if (pid == 0) {
-    FILE *fp = fopen(argv[1], "rb"); // rb for opening non-text files
 
-    /*read in size of matrix*/
-    fread(&size, sizeof(int), 1, fp);
-    printf("Matrix Size: %d\n", size);
-    num_elements = size * size;
-    num_local_elements = num_elements / np;
-    lo = num_elements - (np * num_local_elements);
+  // Matrix is parsed in the root process (process 0)
+  if (pid == root) {
+    FILE *fp = fopen(filename, "rb");
+    fread(&n, sizeof(int), 1, fp);
+    printf("Matrix Size: %d\n", n);
+    int **matrix = malloc(n * sizeof(int *));
+    for (i = 0; i < n; i++) {
+      matrix[i] = malloc(n * sizeof(int));
+    }
+    matrix = parse_Matrix(filename);
+    print_Matrix(matrix, n);
     printf("Number of processes: %d\n", np);
-    printf("Number of elements per processor: %d\n", num_local_elements);
+    printf("%d\n", matrix[0][1]);
+    floyd(matrix, n, pid, np);
   }
 
-  //-----------BROADCAST SIZES---------------
-  MPI_Bcast(&size, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(&num_local_elements, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(&lo, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-  matrix = malloc((size*size) * sizeof(int));
+  //printf("%d\n", matrix[0][1]);
 
-  //------get input matrix---------
-  if (pid == 0) {
-    //parse adjacency matrix (zeroes are converted to 'infinity')
-    MatrixContainer matrix_container = get_Matrix(argv[1], size);
-    matrix = matrix_container.matrix;
-    //print_matrix(matrix, size);
-  }
-
-  double t_start = MPI_Wtime();
-  //---------------PARTITION MATRIX------------------
-  sub_array = malloc(num_local_elements * sizeof(int));
-  MPI_Scatter(&matrix[lo], num_local_elements, MPI_INT, sub_array, num_local_elements, MPI_INT, 0, MPI_COMM_WORLD);
-  int global_index = lo + num_local_elements*pid;
-  //printf("Process %d has %d elements starting at index %d\n", pid, num_local_elements, global_index);
-
-
-  floyd(sub_array, size, pid, np);
-
-  int *temp_matrix = malloc((size * size) * sizeof(int));
-  MPI_Gather(sub_array, num_local_elements, MPI_INT, temp_matrix, num_local_elements, MPI_INT, 0, MPI_COMM_WORLD);
-
-  if (pid == 0) {
-    print_matrix(temp_matrix, size);
-  }
-
-  free(temp_matrix);
-  free(sub_array);
 
   MPI_Barrier(MPI_COMM_WORLD);
   MPI_Finalize();
@@ -85,20 +64,68 @@ int main(int argc, char* argv[]) {
   return 0;
 }
 
-void print_matrix(int *matrix, int size) {
+// Distributed version of Floyd Marshall algorithm
+void floyd(int **matrix, int n, int pid, int p) {
+  int i, j, k;
+  int offset; /* Local index of broadcast row */
+  int root; /* Process controlling row to be bcast */
+  int *temp; /* Holds the broadcast row */
+  temp = malloc(n * sizeof(int));
+
+  for (k = 0; k < n; k++) {
+    root = BLOCK_OWNER(k, p, n);
+    if (root == pid) {
+      offset = k - BLOCK_LOW(pid, p, n);
+      for (j = 0; j < n; j++) {
+        temp[j] = matrix[offset][j];
+      }
+    }
+    MPI_Bcast(temp, n, MPI_INT, root, MPI_COMM_WORLD);
+    for (i = 0; i < BLOCK_SIZE(pid, p, n); i++)
+      for (j = 0; j < n; j++)
+        matrix[i][j] = min(matrix[i][j], matrix[i][k]+temp[j]);
+        printf("%d ", matrix[i][j]);
+  }
+  free(temp);
+}
+
+int **parse_Matrix(char *filename) {
+  int i_elem, i, j, n, **matrix;
+  char type[MAX_LINE_LEN];
+  FILE *fp = fopen(filename, "rb");
+  fread(&n, sizeof(int), 1, fp);
+
+  matrix = malloc(n * sizeof(int *));
+  for (i = 0; i < n; i++) {
+    matrix[i] = malloc(n * sizeof(int));
+  }
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      fread(&i_elem, sizeof(int), 1, fp);
+      if (i == j) i_elem = 0;
+      else if (i_elem == 0) i_elem = INFINITY;
+      matrix[i][j] = i_elem;
+    }
+  }
+  return matrix;
+}
+
+void print_Matrix(int **matrix, int n) {
   int i, j;
-  for (i = 0; i < size; i++) {
-     for (j = 0; j < size; j++) {
-       if (matrix[i*size+j] == INFINITY)
+  for (i = 0; i < n; i++) {
+     for (j = 0; j < n; j++) {
+       if (matrix[i][j] == INFINITY)
           printf("i ");
        else
-          printf("%d ", matrix[i*size+j]);
+          printf("%d ", matrix[i][j]);
      }
      printf("\n");
   }
 }
 
 void print_array(int *sub_array, int size, int np) {
+  int i;
   for (i = 0; i < (size * size)/np; i++) {
     if (sub_array[i] == INFINITY)
       printf("i ");
@@ -108,40 +135,10 @@ void print_array(int *sub_array, int size, int np) {
   printf("\n");
 }
 
-// Distributed version of Floyd Marshall algorithm
-void floyd(int *sub_array, int size, int pid, int np) {
-  int global_k, local_i, global_j, temp;
-  int root;
-  int* row_k = malloc(size * sizeof(int));
-
-  for (global_k = 0; global_k < size; global_k++) {
-    root = owner(global_k, np, size);
-    if (pid == root) {
-      copy_row(sub_array, size, np, row_k, global_k);
-    }
-    MPI_Bcast(row_k, size, MPI_INT, root, MPI_COMM_WORLD);
-    for (local_i = 0; local_i < size/np; local_i++)
-      for (global_j = 0; global_j < size; global_j++) {
-        temp = sub_array[local_i * size + global_k] + row_k[global_j];
-        if (temp < sub_array[local_i * size + global_j])
-           sub_array[local_i * size + global_j] = temp;
-      }
-  }
-  free(row_k);
-}
-
-// Reurn the rank of the process that owns global row k
-int owner(int k, int np, int size) {
-  int pid = k/(size/np);
-  return pid;
-}
-
-// Copy the row with the global subscript k into row_k
-void copy_row(int *sub_array, int size, int np, int *row_k, int global_k) {
-  int j;
-  int local_k = k % (size/np);
-
-  for (j = 0; j < size; j++) {
-    row_k[j] = sub_array[local_k * size + j];
+int min(int a, int b) {
+  if (a<b) {
+    return a;
+  } else {
+    return b;
   }
 }
